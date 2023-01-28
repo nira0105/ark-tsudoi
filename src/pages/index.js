4345a0cef3b89d27d92f91212680cdc16582d522
Niranuranura <113242876+Niranuranura@users.noreply.github.com>
2023-01-28 18:09:30 +0900

    coding: ホームページのスライド壁紙の途中まで実装。

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,10 +10,26 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)} style={{backgroundImage: `url('/img/test_for_hero.jpg')`, backgroundSize: "cover"}}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+    <header id="header" className={clsx('hero hero--primary', styles.heroBanner)} style={{backgroundColor: "transparent"}}>
+      <div class="css-carousel-slider background">
+        <div class="slide-wrap-main">
+            <div class="slide"><img src="/img/test_for_hero.jpg" alt="" /></div>
+        </div>
+        <div class="slide-wrap for-pc">
+            <div class="slide"><img src="/img/hero/pc/1.jpg" alt="" /></div>
+            <div class="slide"><img src="/img/hero/pc/2.png" alt="" /></div>
+            <div class="slide"><img src="img3" alt="" /></div>
+            <div class="slide"><img src="img4" alt="" /></div>
+        </div>
+        <div class="slide-wrap for-sp">
+            <div class="slide"><img src="/img/hero/sp/1.jpg" alt="" /></div>
+            <div class="slide"><img src="/img/hero/sp/2.png" alt="" /></div>
+            <div class="slide"><img src="img3" alt="" /></div>
+            <div class="slide"><img src="img4" alt="" /></div>
+        </div>
+      </div>
+      <div className="container heroContainer">
+        <h1 className="hero__title heroFont">{siteConfig.title}</h1><br /><br />
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
