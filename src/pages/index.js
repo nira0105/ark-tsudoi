93a4ba0059f230727a8405751c1e3f6eaaa78cf9
Takagi Tasuku <tasuren@outlook.jp>
2023-12-20 13:17:35 +0900

    使われていないCSSを削除

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,6 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
-import styles from './index.module.css';
-
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
